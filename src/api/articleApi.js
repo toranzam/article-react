b380dade721b59e896f3826638506557fade3c30
toranzam <nbc05829@gmail.com>
2024-02-04 15:52:16 +0900

    ADD getList api

--- src/api/articleApi.js
+++ src/api/articleApi.js
@@ -1,2 +1,16 @@
+import axios from "axios";
 
 
+const API_SERVER_HOST = 'http://localhost:8080'
+
+const prefix = `${API_SERVER_HOST}/api/todo`
+
+export const getList = async (pageParam) => {
+
+    const {page, size} = pageParam
+
+    const res = await axios.get(`${prefix}/list`, {params:{page, size}})
+
+    return res.data
+
+}
