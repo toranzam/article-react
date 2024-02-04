b634ebc9ad4079032a20d06c978f29fc705c8d74
toranzam <nbc05829@gmail.com>
2024-02-04 15:53:40 +0900

    ADD api getList ArticleListPage.js에 출력

--- src/pages/article/ArticleListPage.js
+++ src/pages/article/ArticleListPage.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSearchParams} from "react-router-dom";
+import {getList} from "../../api/articleApi";
+
+
+const getParam = (param, defaultValue) => {
+    if (!param) {
+        return defaultValue;
+    }
+
+    return parseInt(param)
+
+}
+
+const initState = {
+    dtoList: [],
+}
+
 
 function ArticleListPage(props) {
+
+    const [serverData, setServerData] = useState(initState)
+
+    // 쿼리스트링 추출
+    const [queryParams] = useSearchParams()
+
+    const page = getParam(queryParams.get('page'), 1)
+    const size = getParam(queryParams.get('size'), 10)
+
+
+    useEffect(() => {
+
+        getList({page, size})
+            .then(res => {
+                console.log(res)
+                setServerData(res)
+
+            })
+
+    }, [page, size]);
+
+
     return (
         <table className="table table-striped">
             <thead>
@@ -12,12 +51,14 @@ function ArticleListPage(props) {
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <th className={'col-1'}>1</th>
-                <td className={'col-7'}>Mark</td>
-                <td className={'col-2'}>Otto</td>
-                <td className={'col-2'}>@mdo</td>
-            </tr>
+            {serverData.dtoList.map(article =>
+                <tr key={article.id}>
+                    <th className={'col-1'}>{article.id}</th>
+                    <td className={'col-7'}>{article.title}</td>
+                    <td className={'col-2'}>{article.writer}</td>
+                    <td className={'col-2'}>{article.dueDate}</td>
+                </tr>
+            )}
 
             </tbody>
         </table>
