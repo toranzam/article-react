fe607941f51074915c76c3eb642f7dd0438fe934
toranzam <nbc05829@gmail.com>
2024-02-10 18:23:55 +0900

    Add postAdd api

--- src/api/articleApi.js
+++ src/api/articleApi.js
@@ -1,15 +1,24 @@
 import axios from "axios";
+import async from "async";
 
 
 const API_SERVER_HOST = 'http://localhost:8080'
 
-const prefix = `${API_SERVER_HOST}/api/todo`
+const prefix = `${API_SERVER_HOST}/api/article`
 
 export const getList = async (pageParam) => {
 
     const {page, size} = pageParam
 
     const res = await axios.get(`${prefix}/list`, {params:{page, size}})
+
+    return res.data
+
+}
+
+export const postAdd = async (article) => {
+
+    const res = await axios.post(`${prefix}`, article)
 
     return res.data
 
